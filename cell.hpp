15bfbbbb110d8cb5f9e0e95dad11c8b9d6aba5a9
pascalhuber <pascalkimhuber@aol.com>
2011-11-19 14:24:37 +0100

    Added some data structure to class Cell
    The class contains now:
    - cell_pos : position of cell
    - cell_size: size of cell
    - cell_particles : number of particles in the cell

--- cell.hpp
+++ cell.hpp
@@ -7,8 +7,16 @@
 class Cell 
 {
 public: 
+
+  // data structure
   /// The cell contains particles ...
   std::vector<Particle> particles; 
+  /// position of the cell
+  unsigned cell_pos;
+  /// size of the cell
+  real cell_size[DIM];
+  /// number of particles in cell
+  unsigned cell_particles; 
 }
 
 #endif _CELL_HPP
