4bb92a8e5ce32a992f8cac48a57369ceef52cd12
pascalhuber <pascalkimhuber@aol.com>
2011-11-19 21:10:50 +0100

    Added read_Parameter and read_Particles declaration with short documentation for the doxyfile.

--- world_lc.hpp
+++ world_lc.hpp
@@ -13,6 +13,26 @@ class World_LC : public World {
 public: 
   World_LC(); 
 
+  /**
+   * @brief read world parameters from given parameter file
+   * (*.parameter). In addition to the original member function,
+   * read_parameters now also reads r_cut and calculates all derived
+   * data. 
+   *
+   * @param filename filename of the parameter file
+   */
+  virtual read_Parameter(const std::string &filename);
+
+  /**
+   * @brief read the particles from given particles file
+   * (*.particles).
+   * In addition to the original member function, read_particles
+   * now distributes the particles over the cells. 
+   *
+   * @param filename filename of the particles file
+   */
+  virtual read_Particles(const std::string &filename); 
+
   /// additional data structure for linked cell
   // cells
   std::vector<Cell> cells; 
