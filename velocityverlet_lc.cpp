ead5b157e2be518abee9161f8a5a0d32a22d2dc3
pascalhuber <pascalkimhuber@aol.com>
2011-11-23 21:17:38 +0100

    Tried to write comp_F_in. Couldn't solve the problem til now. Don't know how to choose the right neighbour cells...

--- velocityverlet_lc.cpp
+++ velocityverlet_lc.cpp
@@ -5,3 +5,10 @@ VelocityVerlet_LC::VelocityVerlet_LC(World_LC& _W, Potential& _Pot, Observer &_O
 {
   // empty constructor
 }
+
+// calculate all forces affecting the particles in one specific cell
+// of the world
+void VelocityVerlet_LC::F_comp_in(Cell& C)
+{
+  
+}
